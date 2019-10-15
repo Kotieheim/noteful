37d40f9dc2ba3092f27810eed235bac404d57926
Dakota Heim <kotieheim@gmail.com>
2019-10-15 09:30:21 -0600

    Consulted solution page

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,75 +1,49 @@
-import React, { Component } from "react";
-import NotefulForm from "../NotefulForm/NotefulForm";
-import ApiContext from "../ApiContext";
-import config from "../config";
-import "./AddNote.css";
+import React, { Component } from 'react'
+import NotefulForm from '../NotefulForm/NotefulForm'
+import './AddNote.css'
 
 export default class AddNote extends Component {
   static defaultProps = {
-    history: {
-      push: () => {}
-    }
-  };
-  static contextType = ApiContext;
-
-  handleSubmit = e => {
-    e.preventDefault();
-    const newNote = {
-      name: e.target["note-name"].value,
-      content: e.target["note-content"].value,
-      folderId: e.target["note-folder-id"].value,
-      modified: new Date()
-    };
-    fetch(`${config.API_ENDPOINT}/notes`, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json"
-      },
-      body: JSON.stringify(newNote)
-    })
-      .then(res => {
-        if (!res.ok) return res.json().then(e => Promise.reject(e));
-        return res.json();
-      })
-      .then(note => {
-        this.context.addNote(note);
-        this.props.history.push(`/folder/${note.folderId}`);
-      })
-      .catch(error => {
-        console.error({ error });
-      });
-  };
-
+    folders: [],
+  }
   render() {
-    const { folders = [] } = this.context;
+    const { folders } = this.props
     return (
-      <section className="AddNote">
+      <section className='AddNote'>
         <h2>Create a note</h2>
-        <NotefulForm onSubmit={this.handleSubmit}>
-          <div className="field">
-            <label htmlFor="note-name-input">Name</label>
-            <input type="text" id="note-name-input" name="note-name" />
+        <NotefulForm>
+          <div className='field'>
+            <label htmlFor='note-name-input'>
+              Name
+            </label>
+            <input type='text' id='note-name-input' />
           </div>
-          <div className="field">
-            <label htmlFor="note-content-input">Content</label>
-            <textarea id="note-content-input" name="note-content" />
+          <div className='field'>
+            <label htmlFor='note-content-input'>
+              Content
+            </label>
+            <textarea id='note-content-input' />
           </div>
-          <div className="field">
-            <label htmlFor="note-folder-select">Folder</label>
-            <select id="note-folder-select" name="note-folder-id">
+          <div className='field'>
+            <label htmlFor='note-folder-select'>
+              Folder
+            </label>
+            <select id='note-folder-select'>
               <option value={null}>...</option>
-              {folders.map(folder => (
+              {folders.map(folder =>
                 <option key={folder.id} value={folder.id}>
                   {folder.name}
                 </option>
-              ))}
+              )}
             </select>
           </div>
-          <div className="buttons">
-            <button type="submit">Add note</button>
+          <div className='buttons'>
+            <button type='submit'>
+              Add note
+            </button>
           </div>
         </NotefulForm>
       </section>
-    );
+    )
   }
 }
--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -1,46 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Note from "../Note/Note";
-import CircleButton from "../CircleButton/CircleButton";
-import ApiContext from "../ApiContext";
-import { getNotesForFolder } from "../notes-helpers";
-import "./NoteListMain.css";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Note from '../Note/Note'
+import CircleButton from '../CircleButton/CircleButton'
+import './NoteListMain.css'
 
-export default class NoteListMain extends React.Component {
-  static defaultProps = {
-    match: {
-      params: {}
-    }
-  };
-  static contextType = ApiContext;
+export default function NoteListMain(props) {
+  return (
+    <section className='NoteListMain'>
+      <ul>
+        {props.notes.map(note =>
+          <li key={note.id}>
+            <Note
+              id={note.id}
+              name={note.name}
+              modified={note.modified}
+            />
+          </li>
+        )}
+      </ul>
+      <div className='NoteListMain__button-container'>
+        <CircleButton
+          tag={Link}
+          to='/add-note'
+          type='button'
+          className='NoteListMain__add-note-button'
+        >
+          <FontAwesomeIcon icon='plus' />
+          <br />
+          Note
+        </CircleButton>
+      </div>
+    </section>
+  )
+}
 
-  render() {
-    const { folderId } = this.props.match.params;
-    const { notes = [] } = this.context;
-    const notesForFolder = getNotesForFolder(notes, folderId);
-    return (
-      <section className="NoteListMain">
-        <ul>
-          {notesForFolder.map(note => (
-            <li key={note.id}>
-              <Note id={note.id} name={note.name} modified={note.modified} />
-            </li>
-          ))}
-        </ul>
-        <div className="NoteListMain__button-container">
-          <CircleButton
-            tag={Link}
-            to="/add-note"
-            type="button"
-            className="NoteListMain__add-note-button"
-          >
-            <FontAwesomeIcon icon="plus" />
-            <br />
-            Note
-          </CircleButton>
-        </div>
-      </section>
-    );
-  }
+NoteListMain.defaultProps = {
+  notes: [],
 }
